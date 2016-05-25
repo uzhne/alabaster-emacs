69cd378b433f88060d725f6a332b6f7e831e7f44
Chris Etheridge <chris.etheridge@cognician.com>
2016-05-25 09:08:31 +0200

    Init

---
+++ alabaster-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; espresso-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "espresso-theme" "espresso-theme.el" (22340
+;;;;;;  14165 0 0))
+;;; Generated autoloads from espresso-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; espresso-theme-autoloads.el ends here
